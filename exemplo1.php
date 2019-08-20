7a3e0287a92011e3802e56c2811a54787e7d3ef7
Angelino Rebelo <machaddoodocs@gmail.com>
2019-08-20 11:26:14 -0300

    novo proj

---
+++ exemplo1.php
@@ -0,0 +1,9 @@
+<?php
+/*
+$anoNascimento = 1990;
+$nomeCompleto = ""; */
+//na Linha de baixo temos uma variável com número no nome
+$nome1 = "João";
+echo "<br/";
+
+?>
---
+++ exemplo4.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = (int)$_GET{"a"};
+
+//var_dump ($nome);
+
+///echo ($nome);
+
+//$ip = $_SERVER["REMOTE_ADDR"];
+//$ip = $_SERVER["SCRIPT_NAME"];
+
+echo $ip;
+
+
+
+
+?>
